43a8cd306be7da2abebf9d43138c553d01ab840e
Sebastian Brandes Kraaijenzank <sb@sebastianbrandes.com>
2015-05-19 10:23:14 +0200

    Initial commit

---
+++ src/WebApplication13/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using WebApplication13.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication13.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var post = new BlogPost
+            {
+                Id = 123,
+                Title = "Hello ASP.NET 5",
+                Content = "ASP.NET 5 styrer!",
+                DateTimePosted = DateTime.Now
+            };
+
+            return View("ShowPost2", post);
+        }
+    }
+}
---
+++ src/WebApplication13/Models/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication13.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateTimePosted { get; set; }
+    }
+}
